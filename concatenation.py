f7bb37570d88ad75b8cd71322c306d4edfecad22
MarcosGad <marcosgad@yahoo.com>
2023-10-14 11:33:55 +0200

    +

---
+++ concatenation.py
@@ -0,0 +1,20 @@
+msg = "I Love"
+lang = "Python"
+print(msg + " " + lang) # I Love Python
+
+full = msg + " " + lang
+print(full) # I Love Python
+
+a = "First \
+Second \
+Third"
+
+b = "A \
+B \
+C"
+
+print(a + "\n" + b) 
+# First Second Third
+# A B C
+
+print("Hello " + 1)  # Error
